3af0b1aea9faa6ca369dbf208e45918c3319718f
a595848474 <a595848474@gmail.com>
2015-12-11 11:37:44 -0500

    Add a test commit

--- shiny/ui.R
+++ shiny/ui.R
@@ -54,7 +54,8 @@ shinyUI(navbarPage("Welcome to the Titanic!",
                 sliderInput("fare", label = h4("Passenger Fare"), 
                             min = 0, max = 513, value = 50),
                 
-                submitButton("Submit")
+                submitButton("Submit!")
+                
      
               ),
               mainPanel("")
