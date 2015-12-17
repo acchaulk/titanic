1781078d50fdf1f3fe369b350569335f299e19b0
Adam Chaulk <acchaulk@gmail.com>
2015-12-17 10:54:39 -0500

    Added images, made results only appear on click

--- shiny/server.R
+++ shiny/server.R
@@ -1,109 +1,136 @@
 library(shiny)
 library(randomForest)
+library(party)
 
 shinyServer(function(input, output) {
-  # titanic.passenger <-
+  rb <- reactive ({
+    titanic.passenger <-
+      data.frame(
+        Pclass = input$pclass,  Sex = input$sex, Age = input$age,
+        SibSp = input$sibsp, Parch = input$parch, Fare = input$fare,
+        Embarked = input$embarked, Title = input$title
+      )
     
-    rb <- reactive ({
-      
-#       data.frame(
-#         Name = c("Integer", 
-#                  "Decimal",
-#                  "Range",
-#                  "Custom Format",
-#                  "Animation"),
-#         Value = as.character(c(input$name, 
-#                                input$title,
-#                                input$sex,
-#                                input$sibsp,
-#                                input$parch)), 
-#         stringsAsFactors=FALSE)
-      
-     titanic.passenger <- data.frame(Pclass = input$pclass,  Sex = input$sex, Age = input$age, 
-                                SibSp = input$sibsp, Parch = input$parch, Fare = input$fare, 
-                                Embarked = input$embarked, Title = input$title)
-     
-     titanic.passenger$FamilySize <- titanic.passenger$SibSp + titanic.passenger$Parch + 1
-     
-     titanic.passenger$FamilySize <- titanic.passenger$FamilySize
-     
-     titanic.passenger$FamilyID <- paste(as.character(titanic.passenger$FamilySize), input$last, sep = "")
-     
-     titanic.passenger$Survived <- NA
-     
-     # build the cforest model
-     
-     # Conditional Inference forest
-     library(party)
-    # titanic.im.train.cf <- cforest(Survived ~ ., data = titanic.im.train[, -c(2, 5, 7, 12)], controls=cforest_unbiased(ntree=2000, mtry=3))
-     
-     
-     titanic.passenger$Age <- as.numeric(titanic.passenger$Age)
-     
-     titanic.passenger$Embarked <- factor(titanic.passenger$Embarked, levels = levels(titanic.im.train$Embarked))
-    # levels(titanic.passenger$Embarked) <- levels(titanic.im.train$Embarked)
-     
-     titanic.passenger$Fare <- as.numeric(titanic.passenger$Fare)
-     
-     titanic.passenger$Parch <- as.integer(titanic.passenger$Parch)
-     
-     titanic.passenger$Pclass <- factor(titanic.passenger$Pclass, levels = levels(titanic.im.train$Pclass))
-     #levels(titanic.passenger$Pclass) <- levels(titanic.im.train$Pclass)
-     
-     titanic.passenger$Sex <- factor(titanic.passenger$Sex, levels = levels(titanic.im.train$Sex))
-    # levels(titanic.passenger$Sex) <- levels(titanic.im.train$Sex)
-     
-     titanic.passenger$SibSp <- as.integer(titanic.passenger$SibSp)
-     
-     titanic.passenger$Survived <- factor(titanic.passenger$Survived, levels = levels(titanic.im.train$Survived))
-    # levels(titanic.passenger$Survived) <- levels(titanic.im.train$Survived)
-     
-     titanic.passenger$FamilySize <- as.integer(titanic.passenger$FamilySize)
-     
-     titanic.passenger$Title <- factor(titanic.passenger$Title, levels = levels(titanic.im.train$Title))
-    # levels(titanic.passenger$Title) <- levels(titanic.im.train$Title)
-     
-     titanic.passenger$FamilyID <- factor(titanic.passenger$FamilyID, levels = levels(titanic.im.train$FamilyID))
-     #levels(titanic.passenger$FamilyID) <- levels(titanic.im.train$FamilyID)
-     
-   #  testData <- rbind(titanic.im.test[1:10, -c(2, 5, 7, 12)], titanic.passenger)
-     
-    titanic.passenger.pred <- predict(titanic.im.train.cf, newdata=titanic.passenger, type = "response")
-      
-     return(titanic.passenger.pred)
-
-    })
+    titanic.passenger$FamilySize <-
+      titanic.passenger$SibSp + titanic.passenger$Parch + 1
     
-  output$text1 <- renderText({ 
-    if(input$goButton) {
-      var <- rb()
-      # paste(var)
-      if(var == 0) {
-        paste("dead")
-      }
-      
-      else if(var == 1) {
-        paste("survived")
-      }
-     
+    titanic.passenger$FamilySize <- titanic.passenger$FamilySize
+    
+    titanic.passenger$FamilyID <-
+      paste(as.character(titanic.passenger$FamilySize), input$last, sep = "")
+    
+    titanic.passenger$Survived <- NA
+    
+    titanic.passenger$Age <- as.numeric(titanic.passenger$Age)
+    
+    titanic.passenger$Embarked <-
+      factor(titanic.passenger$Embarked, levels = levels(titanic.im.train$Embarked))
+    
+    titanic.passenger$Fare <- as.numeric(titanic.passenger$Fare)
+    
+    titanic.passenger$Parch <- as.integer(titanic.passenger$Parch)
+    
+    titanic.passenger$Pclass <-
+      factor(titanic.passenger$Pclass, levels = levels(titanic.im.train$Pclass))
+    
+    titanic.passenger$Sex <-
+      factor(titanic.passenger$Sex, levels = levels(titanic.im.train$Sex))
+    
+    titanic.passenger$SibSp <- as.integer(titanic.passenger$SibSp)
+    
+    titanic.passenger$Survived <-
+      factor(titanic.passenger$Survived, levels = levels(titanic.im.train$Survived))
+    
+    titanic.passenger$FamilySize <-
+      as.integer(titanic.passenger$FamilySize)
+    
+    titanic.passenger$Title <-
+      factor(titanic.passenger$Title, levels = levels(titanic.im.train$Title))
+    
+    titanic.passenger$FamilyID <-
+      factor(titanic.passenger$FamilyID, levels = levels(titanic.im.train$FamilyID))
+    
+    titanic.passenger.pred <-
+      predict(titanic.im.train.cf, newdata = titanic.passenger, type = "response")
+    
+    return(titanic.passenger.pred)
+    
+  })
+  
+  output$survivedText <- renderText({
+    if (input$goButton == 0) {
+      return("")
     }
-    })
     
+    isolate({
+      res <- rb()
+      if (res == 0) {
+        paste("You died!")
+      }
+      
+      else if (res == 1) {
+        paste("You survived!")
+      }
+      
+    })
+  })
+  
+  output$survivalImage <- renderImage({
+    if (input$goButton == 0) {
+      return(
+        list(
+          src = "titanic-default.jpg",
+          contentType = "image/jpg",
+          height = 500, width = 700,
+          alt = "Unavailable"
+        )
+      )
+    }
+    
+    isolate({
+      res <- rb()
+      if (res == 0) {
+        return(
+          list(
+            src = "died.jpg",
+            contentType = "image/jpg",
+            height = 500, width = 700,
+            alt = "Face"
+          )
+        )
+      }
+      else if (res == 1) {
+        return(
+          list(
+            src = "survived.jpg",
+            filetype = "image/jpg",
+            height = 500, width = 700,
+            alt = "This is a chainring"
+          )
+        )
+      }
+    })
+  }, deleteFile = FALSE)
+  
+  
   
   output$survivalByClass <- renderPlot({
     # credit: https://www.kaggle.com/slothouber/titanic/traveling-alone-or-with-family
     train <- read.csv("train.csv")
     test  <- read.csv("test.csv")
-
+    
     # Here we will plot the passenger survival by class
-    train$Survived <- factor(train$Survived, levels=c(1,0))
+    train$Survived <- factor(train$Survived, levels = c(1,0))
     levels(train$Survived) <- c("Survived", "Died")
     train$Pclass <- as.factor(train$Pclass)
     levels(train$Pclass) <- c("1st Class", "2nd Class", "3rd Class")
     
-    mosaicplot(train$Pclass ~ train$Survived, main="Passenger Survival by Class",
-               color=c("#8dd3c7", "#fb8072"), shade=FALSE,  xlab="", ylab="",
-               off=c(0), cex.axis=1.4)
+    mosaicplot(
+      train$Pclass ~ train$Survived, main = "Passenger Survival by Class",
+      color = c("#8dd3c7", "#fb8072"), shade = FALSE,  xlab = "", ylab =
+        "",
+      off = c(0), cex.axis = 1.4
+    )
   })
   
   output$survivalByGender <- renderPlot({
@@ -112,14 +139,17 @@ shinyServer(function(input, output) {
     test  <- read.csv("test.csv")
     
     # Here we will plot the passenger survival by class
-    train$Survived <- factor(train$Survived, levels=c(1,0))
+    train$Survived <- factor(train$Survived, levels = c(1,0))
     levels(train$Survived) <- c("Survived", "Died")
     train$Pclass <- as.factor(train$Pclass)
     levels(train$Pclass) <- c("1st Class", "2nd Class", "3rd Class")
     
-    mosaicplot(train$Sex ~ train$Survived, 
-               main="Passenger Fate by Gender", shade=FALSE, color=c("#8dd3c7", "#fb8072"),
-               xlab="Sex", ylab="Survived")
+    mosaicplot(
+      train$Sex ~ train$Survived,
+      main = "Passenger Survival by Gender", shade = FALSE, color =
+        c("#8dd3c7", "#fb8072"),
+      xlab = "Sex", ylab = "Survived"
+    )
   })
   
   output$survivalByAge <- renderPlot ({
@@ -128,14 +158,16 @@ shinyServer(function(input, output) {
     test  <- read.csv("test.csv")
     
     # Here we will plot the passenger survival by class
-    train$Survived <- factor(train$Survived, levels=c(1,0))
+    train$Survived <- factor(train$Survived, levels = c(1,0))
     levels(train$Survived) <- c("Survived", "Died")
     train$Pclass <- as.factor(train$Pclass)
     levels(train$Pclass) <- c("1st Class", "2nd Class", "3rd Class")
     
-    boxplot(train$Age ~ train$Survived, 
-            main="Passenger Fate by Age",
-            xlab="Survived", ylab="Age")
+    boxplot(
+      train$Age ~ train$Survived,
+      main = "Passenger Survival by Age",
+      xlab = "Survived", ylab = "Age"
+    )
   })
   
   output$survivalByEmbarkation <- renderPlot ({
@@ -144,14 +176,16 @@ shinyServer(function(input, output) {
     test  <- read.csv("test.csv")
     
     # Here we will plot the passenger survival by class
-    train$Survived <- factor(train$Survived, levels=c(1,0))
+    train$Survived <- factor(train$Survived, levels = c(1,0))
     levels(train$Survived) <- c("Survived", "Died")
     train$Pclass <- as.factor(train$Pclass)
     levels(train$Pclass) <- c("1st Class", "2nd Class", "3rd Class")
     
-    mosaicplot(train$Embarked ~ train$Survived, 
-               main="Passenger Fate by Port of Embarkation",
-               shade=FALSE, color=c("#8dd3c7", "#fb8072"), xlab="Embarked", ylab="Survived")
+    mosaicplot(
+      train$Embarked ~ train$Survived,
+      main = "Passenger Survival by Port of Embarkation",
+      shade = FALSE, color = c("#8dd3c7", "#fb8072"), xlab = "Embarked", ylab =
+        "Survived"
+    )
   })
-  
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,98 +1,119 @@
 library(shiny)
 
-shinyUI(navbarPage("Welcome to the Titanic!",
-   tabPanel("Will you survive?",
-            titlePanel("Enter your information"),
-            sidebarLayout(
-              sidebarPanel(
-                
-                fluidRow(
-                  column(4, 
-                    textInput("first", label = h4("First Name"), 
-                              value = "")),
-                  
-                  column(4, 
-                         textInput("last", label = h4("Last Name"), 
-                                   value = "")),
-                  
-                  column(4, 
-#                         Title mapping:  {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 6, "Major": 7, "Col": 7, "Mlle": 8, "Mme": 8, "Don": 9, "
-#                            Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9, "Capt": 7, "Ms": 2}
-                    selectInput("title", label = h4("Title"), 
-                                  choices = list("Mr" = 1, "Miss" = 2, "Mrs" = 3, "Master" = 4, 
-                                                 "Dr" = 5, "Rev" = 6, "Major" = 7, "Col" = 7, "Mlle" = 8, 
-                                                 "Mme" = 8, "Don" = 9, "Lady" = 10, "Countess" = 10,
-                                                 "Jonkheer" = 10, "Sir" = 9,"Capt" = 7, "Ms" = 2),
-                                selected = 1))),
-                
-                fluidRow(
-                  column(6, 
-                    radioButtons("sex", label = h4("Gender"), 
-                                choices = list("Male" = 0, "Female" = 1),
-                                selected = 0)),
-                  column(6, 
-                    numericInput("age", 
-                                label = h4("Age"), 
-                                value = 1))),
-            
-                fluidRow(
-                  column(6, 
-                    numericInput("sibsp", 
-                                 label = h4("Number of Siblings and Spouses Aboard"), 
-                                 value = 0)),
-                
-                  column(6, 
-                    numericInput("parch", 
-                                 label = h4("Number of Parents and Children Aboard"), 
-                                 value = 0))),
-                
-                fluidRow(
-                  column(6, 
-                    radioButtons("embarked", label = h4("Point of Embarkation"), 
-                                 choices = list("Cherbourg" = 1, "Queenstown" = 2, "Southampton" = 0),
-                                 selected = 1)),
-      
-                  column(6, 
-                    radioButtons("pclass", label = h4("Passenger Class"), 
-                                 choices = list("Upper" = 1, "Middle" = 2, "Lower" = 3),
-                                 selected = 1))),
-                
-                sliderInput("fare", label = h4("Passenger Fare"), 
-                            min = 0, max = 513, value = 50),
-                
-               # submitButton("Submit!")
-              actionButton("goButton", "Go")
-                
-     
-              ),
-              mainPanel(
-                h1(textOutput("text1"))
-              )
-            )
-    ),
-   navbarMenu("Visualizations",
-    tabPanel("Passenger Survival by Class",
-      mainPanel(
-        plotOutput("survivalByClass")
-      )
-    ),
-    tabPanel("Passenger Survival by Gender",
-             mainPanel(
-               plotOutput("survivalByGender")
-             )
-    ),
-    tabPanel("Passenger Survival by Age",
-             mainPanel(
-               plotOutput("survivalByAge")
-             )
-    ),
-    tabPanel("Passenger Survival by Embarkation",
-             mainPanel(
-               plotOutput("survivalByEmbarkation")
-             )
-    ),
-    tabPanel("Lucky Names",
-       mainPanel("Vis B")
+shinyUI(navbarPage(
+  "Welcome to the Titanic!",
+  tabPanel(
+    "Will you survive?",
+    titlePanel("Enter your information"),
+    sidebarLayout(
+      sidebarPanel(
+        fluidRow(
+          column(4,
+                 textInput(
+                   "first", label = h4("First Name"),
+                   value = ""
+                 )),
+          
+          column(4,
+                 textInput(
+                   "last", label = h4("Last Name"),
+                   value = ""
+                 )),
+          
+          column(4,
+                 selectInput(
+                   "title", label = h4("Title"),
+                   choices = list(
+                     "Mr" = 1, "Miss" = 2, "Mrs" = 3, "Master" = 4,
+                     "Dr" = 5, "Rev" = 6, "Major" = 7, "Col" = 7, "Mlle" = 8,
+                     "Mme" = 8, "Don" = 9, "Lady" = 10, "Countess" = 10,
+                     "Jonkheer" = 10, "Sir" = 9,"Capt" = 7, "Ms" = 2
+                   ),
+                   selected = 1
+                 ))
+        ),
+        
+        fluidRow(column(
+          6,
+          radioButtons(
+            "sex", label = h4("Gender"),
+            choices = list("Male" = 0, "Female" = 1),
+            selected = 0
+          )
+        ),
+        column(
+          6,
+          numericInput(
+            "age",
+            label = h4("Age"),
+            min = 1,
+            value = 1
+          )
+        )),
+        
+        fluidRow(column(
+          6,
+          numericInput(
+            "sibsp",
+            label = h4("Number of Siblings and Spouses Aboard"),
+            min = 0,
+            value = 0
+          )
+        ),
+        
+        column(
+          6,
+          numericInput(
+            "parch",
+            label = h4("Number of Parents and Children Aboard"),
+            min = 0,
+            value = 0
+          )
+        )),
+        
+        fluidRow(column(
+          6,
+          radioButtons(
+            "embarked", label = h4("Point of Embarkation"),
+            choices = list(
+              "Cherbourg" = 1, "Queenstown" = 2, "Southampton" = 0
+            ),
+            selected = 1
+          )
+        ),
+        
+        column(
+          6,
+          radioButtons(
+            "pclass", label = h4("Passenger Class"),
+            choices = list(
+              "Upper" = 1, "Middle" = 2, "Lower" = 3
+            ),
+            selected = 1
+          )
+        )),
+        
+        sliderInput(
+          "fare", label = h4("Passenger Fare"),
+          min = 0, max = 513, value = 50
+        ),
+        actionButton("goButton", "Go")
+      ),
+      mainPanel(h1(textOutput("survivedText")),
+                imageOutput("survivalImage"))
     )
-   )
+  ),
+  navbarMenu(
+    "Visualizations",
+    tabPanel("Passenger Survival by Class",
+             mainPanel(plotOutput("survivalByClass"))),
+    tabPanel("Passenger Survival by Gender",
+             mainPanel(plotOutput("survivalByGender"))),
+    tabPanel("Passenger Survival by Age",
+             mainPanel(plotOutput("survivalByAge"))),
+    tabPanel("Passenger Survival by Embarkation",
+             mainPanel(plotOutput(
+               "survivalByEmbarkation"
+             )))
+  )
 ))
